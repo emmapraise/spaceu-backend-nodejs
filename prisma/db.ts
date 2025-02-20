0d364e70d907372087aab33a8f7200e01b6671e5
Emmapraise <emmapraise@gmail.com>
2025-02-20 09:20:33 +0100

    Space model

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,5 +1,5 @@
 import { Prisma, PrismaClient } from '@prisma/client';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 declare let global: {
 	prisma: PrismaClient;
@@ -7,31 +7,32 @@ declare let global: {
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient()
-  } else if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
-    dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' });
-    prisma = new PrismaClient()
-  }
-  else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient()
-    }
-  
-    prisma = global.prisma
-  
-    prisma.$extends({
-  
-      model : {
-        $allModels : {
-          async getModelName<T>(
-              this: T
-          ) {
-            return Prisma.getExtensionContext(this)
-          }
-        }
-      }
-    })
-  }
-  
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient();
+} else if (
+	process.env.NODE_ENV === 'test' ||
+	process.env.NODE_ENV === 'development'
+) {
+	dotenv.config({
+		path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+	});
+	prisma = new PrismaClient();
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
+
+	prisma = global.prisma;
+
+	prisma.$extends({
+		model: {
+			$allModels: {
+				async getModelName<T>(this: T) {
+					return Prisma.getExtensionContext(this);
+				},
+			},
+		},
+	});
+}
+
 export default prisma;
