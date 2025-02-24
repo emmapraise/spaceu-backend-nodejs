c94a92f524de7bb44110084e5204bc84c31dc889
Emmapraise <emmapraise@gmail.com>
2025-02-24 11:24:30 +0100

    Upload

--- src/Libs/Utils/uploadHelper.ts
+++ src/Libs/Utils/uploadHelper.ts
@@ -3,31 +3,31 @@ import upload from '../Middleware/multer';
 import cloudinary from './uploadMedia';
 
 export const handleFileUpload = (
-    req: Request,
-    res: Response
+	req: Request,
+	res: Response
 ): Promise<string> => {
-    return new Promise((resolve, reject) => {
-        upload.single('file')(req, res, (err: any) => {
-            if (err) {
-                reject({ status: 400, message: err.message });
-            } else if (!req.file) {
-                reject({ status: 400, message: 'No file uploaded' });
-            } else {
-                resolve(req.file.path);
-            }
-        });
-    });
+	return new Promise((resolve, reject) => {
+		upload.single('file')(req, res, (err: any) => {
+			if (err) {
+				reject({ status: 400, message: err.message });
+			} else if (!req.file) {
+				reject({ status: 400, message: 'No file uploaded' });
+			} else {
+				resolve(req.file.path);
+			}
+		});
+	});
 };
 
 export const uploadToCloudinary = async (req: Request, res: Response) => {
-    const filePath = await handleFileUpload(req, res);
-    // console.log(filePath);
+	const filePath = await handleFileUpload(req, res);
+	// console.log(filePath);
 
-    return await cloudinary.uploader.upload(filePath, {
-        folder: 'spaceU',
-        // public_id: 'dhnlhnfdnhlndh',
-        fetch_format: 'auto',
-        quality: 'auto',
-        tags: ['space'],
-    });
+	return await cloudinary.uploader.upload(filePath, {
+		folder: 'spaceU',
+		// public_id: 'dhnlhnfdnhlndh',
+		fetch_format: 'auto',
+		quality: 'auto',
+		tags: ['space'],
+	});
 };
